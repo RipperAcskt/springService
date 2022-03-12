609b3046a4afdb6c5e8d5513ca402f47f941bd07
RipperAcskt <7309333@gmail.com>
2022-03-12 20:07:01 +0300

    add logging and unit tests

--- src/main/java/com/example/springservice/controller/Controll.java
+++ src/main/java/com/example/springservice/controller/Controll.java
@@ -3,9 +3,10 @@ package com.example.springservice.controller;
 import com.example.springservice.exception.Exception;
 import com.example.springservice.polindrom.Polindrom;
 import com.example.springservice.response.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,14 +16,23 @@ import java.net.BindException;
 
 @RestController
 public class Controll {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Controll.class);
+
     @GetMapping("/")
     void error() throws Exception{
+        LOGGER.info("Endpoint: /");
+        LOGGER.info("Internal Server Error");
         throw new Exception("Internal Server Error", HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @GetMapping("/check")
     Polindrom len(@RequestParam(value = "str", defaultValue = "") String word) throws Exception {
-        if(word.equals("")) throw new Exception("BAD REQUEST", HttpStatus.BAD_REQUEST);
+        LOGGER.info("Endpoint: /cheak");
+        LOGGER.info("Request: " + word);
+        if(word.equals("")){
+            LOGGER.info("BAD REQUEST");
+            throw new Exception("BAD REQUEST", HttpStatus.BAD_REQUEST);
+        }
 
         boolean check = true;
         String str = "Yes";
@@ -33,11 +43,13 @@ public class Controll {
             }
         }
         if(!check) str = "No";
+        LOGGER.info("Response: len: " + String.valueOf(word.length()) + " isPolinom: " + str);
         return new Polindrom(word.length(), str);
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Response> handleException(Exception e){
+        LOGGER.info("Exception: " + e.getException());
         Response response = new Response(e.getException());
 
         return new ResponseEntity<>(response, e.getStatus());
---
+++ src/test/java/com/example/springservice/TestController.java
@@ -0,0 +1,89 @@
+package com.example.springservice;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class TestController {
+    @Autowired
+    WebApplicationContext webApplicationContext;
+    MockMvc mockMvc;
+
+    @BeforeEach
+    public void before(){
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void test1() throws Exception {
+        MvcResult mvcResult = mockMvc
+                .perform(
+                        MockMvcRequestBuilders
+                                .get("/check")
+                                .contentType(MediaType.APPLICATION_XHTML_XML)
+                                .param("str", "123"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print())
+                .andReturn();
+        System.out.println(mvcResult.getResponse().getContentAsString());
+
+    }
+
+    @Test
+    public void test2() throws Exception {
+        MvcResult mvcResult = mockMvc
+                .perform(
+                        MockMvcRequestBuilders
+                                .get("/check")
+                                .contentType(MediaType.APPLICATION_XHTML_XML)
+                                .param("str", ""))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andDo(MockMvcResultHandlers.print())
+                .andReturn();
+        System.out.println(mvcResult.getResponse().getContentAsString());
+
+    }
+
+    @Test
+    public void test3() throws Exception {
+        MvcResult mvcResult = mockMvc
+                .perform(
+                        MockMvcRequestBuilders
+                                .get("/check")
+                                .contentType(MediaType.APPLICATION_XHTML_XML))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andDo(MockMvcResultHandlers.print())
+                .andReturn();
+        System.out.println(mvcResult.getResponse().getContentAsString());
+
+    }
+
+    @Test
+    public void test4() throws Exception {
+        MvcResult mvcResult = mockMvc
+                .perform(
+                        MockMvcRequestBuilders
+                                .get("/")
+                                .contentType(MediaType.APPLICATION_XHTML_XML))
+                .andExpect(MockMvcResultMatchers.status().isInternalServerError())
+                .andDo(MockMvcResultHandlers.print())
+                .andReturn();
+        System.out.println(mvcResult.getResponse().getContentAsString());
+
+    }
+
+}
