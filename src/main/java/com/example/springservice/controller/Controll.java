69fad9e6f7bb1183172f8a1a11de9f6afa8505a3
Ripper <7309333@gmail.com>
2022-04-24 21:51:35 +0300

    ädd post

--- src/main/java/com/example/springservice/controller/Controll.java
+++ src/main/java/com/example/springservice/controller/Controll.java
@@ -8,10 +8,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.LinkedList;
+import java.util.List;
 
 @RestController
 public class Controll {
@@ -38,6 +38,21 @@ public class Controll {
         System.out.println(Thread.currentThread().getName());
         calls.increment();
         return counter.calc(word);
+    }
+
+    @RequestMapping(value = {"/calc"}, method = RequestMethod.POST)
+    public List<Polindrom> calculateBulkParams(@RequestBody List<String> bodyList) {
+        String res = "";
+
+        List<Polindrom> resultList = new LinkedList<>();
+        bodyList.forEach((currentElement) -> {
+            CountLogic count = new CountLogic();
+            resultList.add(count.calc(currentElement));
+        });
+
+        LOGGER.info("Successfully postMapping");
+
+        return resultList;
     }
 
     @ExceptionHandler(Exception.class)
